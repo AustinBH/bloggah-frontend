c2c7ad204eeff699d338efebae4d3ec68405a2e7
AustinBH <harlowaustin2@gmail.com>
2019-09-24 16:22:05 -0700

    added initial header component

---
+++ src/components/SiteHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Icon } from 'semantic-ui-react';
+
+const SiteHeader = props => {
+    return <div className='header-holder'>
+        <h1><Icon name='write' />Bloggah</h1>
+    </div>
+}
+
+export default SiteHeader;
