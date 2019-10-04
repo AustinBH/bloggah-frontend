e92d1e76ca25199441f00865fd42799ad93b755f
AustinBH <harlowaustin2@gmail.com>
2019-10-03 21:43:00 -0700

    added side nav, need to add animation and display/hide functionality + tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import SiteHeader from './components/SiteHeader';
+import SideNav from './components/SideNav';
 
 function App() {
   return (
     <div className="App-header">
       <SiteHeader/>
+      <SideNav/>
     </div>
   );
 }
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { Menu, Input } from 'semantic-ui-react';
 
 const SideNav = props => {
-    return null
+
+    const [search, setSearch] = useState('')
+
+    const handleChange = ev => {
+        setSearch(ev.target.value)
+    }
+
+    return <Router>
+        <Menu vertical className='side-nav'>
+            <Menu.Item name='Home' as={NavLink} exact to='/' activeClassName='active' content='Home'/>
+            <Menu.Item name='Create New Post' as={NavLink} exact to='/new' activeClassName='active' content='Create New Post' />
+            <Menu.Item name='Search'>
+                <Input icon='search' placeholder='Search posts...' value={search} onChange={handleChange} />
+            </Menu.Item>
+        </Menu>
+        <Route path='/' exact render={props => null}/>
+        <Route path='/new' exact render={props => null}/>
+    </Router>
 }
 
 export default SideNav;
