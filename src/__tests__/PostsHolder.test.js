9e3e7f4b5c1f3b03c90913013fe1cbcc31d4f669
AustinBH <harlowaustin2@gmail.com>
2019-10-31 17:45:00 -0700

    added an initial state test for the posts holder component

--- src/__tests__/PostsHolder.test.js
+++ src/__tests__/PostsHolder.test.js
@@ -1,7 +1,26 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import PostsHolder from '../components/PostsHolder';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 it('renders without crashing', () => {
-    shallow(<PostsHolder />)
+    Enzyme.shallow(<PostsHolder />)
+});
+
+describe("<PostsHolder />", () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = Enzyme.mount(<PostsHolder />);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("has an initial state of posts as an empty array", () => {
+        expect(wrapper.state()).toEqual({ posts: []});
+    });
 });
